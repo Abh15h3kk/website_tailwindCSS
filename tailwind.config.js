fa855bca60ad7e3b57efffd02081573fef1c1fa2
Abh15h3kk <abhiver@iitk.ac.in>
2023-01-11 22:29:03 +0530

    added website data

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+module.exports = {
+  content: ["*"],
+  ttheme: {
+    fontFamily: {
+      'Merriweather': ['Merriweather', 'serif;'],
+      'Montserrat': ['Montserrat','sans-serif;'],
+      'body':['Sacramento','curvise;'],
+    },
+  },
+  plugins: [],
+}
